69be4f72c81bd7edceeddd2a8ebd8d5a14076683
mantis <morauszki.andras@gmail.com>
2024-07-22 13:09:17 +0200

    New API Route & Add text and target to tweet link

--- src/components/QuoteBox/QuoteBox.component.jsx
+++ src/components/QuoteBox/QuoteBox.component.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { GetQuoteButton, QuoteBoxDiv, QuoteBoxTitle } from './QuoteBox.styles';
-import { LogoTwitter } from 'react-ionicons';
-import Quote from '../Quote/Quote.component';
-import SocialLink from '../SocialLink/SocialLink.component';
+import { GetQuoteButton, QuoteBoxDiv, QuoteBoxTitle } from "./QuoteBox.styles";
+import { LogoTwitter } from "react-ionicons";
+import Quote from "../Quote/Quote.component";
+import SocialLink from "../SocialLink/SocialLink.component";
 
 const QuoteBox = () => {
-  const [text, setText] = useState('Your quote is loading');
-  const [author, setAuthor] = useState('');
+  const [text, setText] = useState("Your quote is loading");
+  const [author, setAuthor] = useState("");
 
   const getRandomQuote = async function () {
     try {
-      const res = await fetch('https://api.quotable.io/random');
-      const quote = await res.json();
+      const res = await fetch("https://api.quotable.io/quotes/random");
+      const [quote] = await res.json();
       setText(quote.content);
       setAuthor(quote.author);
     } catch (error) {
@@ -32,7 +32,8 @@ const QuoteBox = () => {
         New Quote
       </GetQuoteButton>
       <SocialLink
-        href="https://twitter.com/intent/tweet"
+        href={`https://twitter.com/intent/tweet?text=${text}%20(${author})`}
+        target="_blank"
         id="tweet-quote"
         data-text={`${text} ${author}`}
       >
